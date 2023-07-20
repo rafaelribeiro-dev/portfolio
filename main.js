bd4f3d16a618d303af427d287e634039bfcc0eb0
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2023-07-20 07:30:16 -0300

    sections select-mode e contact repaginados

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ button.addEventListener('click', () => {
       console.error('Falha ao copiar o texto:', err)
     })
 })
+
+const toggleBtn = document.querySelector('.toggle-btn')
+const moonIcon = document.querySelector('.icon i')
+// const memoji = document.querySelector('.avatar img')
+const avatar = document.querySelector('.avatar')
+
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('active')
+
+  const containsActiveClass = toggleBtn.classList.contains('active')
+  if (containsActiveClass) {
+    avatar.classList.toggle('active')
+
+    // moonIcon.classList.replace('ph-sun', 'ph-moon')
+    // memoji.setAttribute('src', './assets/memoji-without-glasses.svg')
+    return
+  }
+  avatar.classList.toggle('active')
+  // moonIcon.classList.replace('ph-moon', 'ph-sun')
+  // memoji.setAttribute('src', './assets/memoji-glasses.svg')
+})
