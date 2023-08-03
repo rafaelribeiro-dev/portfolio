ae03ccb6f761d864aef86aaa193565f06c12d198
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2023-08-03 07:18:32 -0300

    teste modo dark com localstorage

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let buttonCopy = document.querySelector('.button-copy')
-let checkmark = document.querySelector('.checkmark-svg')
+const buttonCopy = document.querySelector('.button-copy')
+const checkmark = document.querySelector('.checkmark-svg')
 
 const copyEmailToClipboard = () => {
   const input = document.querySelector('.copy-mail')
@@ -14,8 +14,8 @@ const copyEmailToClipboard = () => {
         buttonCopy.style.background = '#6cd86b'
         setTimeout(() => {
           checkmark.style.display = 'none'
-          buttonCopy.textContent = 'Copiar Email'
-          buttonCopy.style.background = ''
+          buttonCopy.textContent = `Copiar Email`
+          buttonCopy.style.background = 'var(--neutral-100)'
         }, 3000)
       }, 100)
       console.log('Texto copiado com sucesso!')
@@ -34,22 +34,43 @@ const sun = document.querySelector('.sun')
 const moon = document.querySelector('.moon')
 const avatar = document.querySelector('.avatar')
 
-toggleBtn.addEventListener('click', () => {
+const body = document.querySelector('body')
+
+let getMode = localStorage.getItem('mode')
+
+if (getMode && getMode === 'dark') {
+  body.classList.add('dark')
+  toggleBtn.classList.add('active')
+}
+
+const handleBackgroundMode = () => {
+  body.classList.toggle('dark')
+  if (!body.classList.contains('dark')) {
+    return localStorage.setItem('mode', 'light')
+  }
+  localStorage.setItem('mode', 'dark')
+}
+
+const handleSwitchMode = () => {
   toggleBtn.classList.toggle('active')
 
   const isActiveClass = toggleBtn.classList.contains('active')
 
   if (isActiveClass) {
     avatar.classList.toggle('active')
-    section.classList.toggle('mode')
+    section.classList.toggle('night')
     sun.style.display = 'none'
     moon.style.display = 'block'
     moonIcon.classList.replace('ph-sun', 'ph-moon')
     return
   }
   avatar.classList.toggle('active')
-  section.classList.remove('mode')
+  section.classList.remove('night')
   moon.style.display = 'none'
   sun.style.display = 'block'
   moonIcon.classList.replace('ph-moon', 'ph-sun')
-})
+}
+
+toggleBtn.addEventListener('click', handleSwitchMode)
+
+toggleBtn.addEventListener('click', handleBackgroundMode)
