31ce58492686e4480ea7578118a210ad6eaf78be
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2024-05-19 12:04:34 -0300

    chore: change style portfolio

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // SWIPER JS CARDS DE APRESENTAÇÃO DOS PROJETOS
 var swiper = new Swiper('.slide-container', {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 10,
   slidesPerGroup: 1,
   loop: true,
   // autoplay: true,
@@ -11,9 +11,9 @@ var swiper = new Swiper('.slide-container', {
   grabCursor: true,
   fade: true,
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
+    // el: '.swiper-pagination',
+    clickable: true
+    // dynamicBullets: true
   },
   navigation: {
     nextEl: '.swiper-button-next',
@@ -35,280 +35,292 @@ var swiper = new Swiper('.slide-container', {
   }
 })
 
-const preload = gsap.timeline({
-  paused: true
-})
-preload
-  .to('.loader_bg', {
-    delay: 0.5,
-    duration: 0.5,
-    opacity: 0,
-    zIndex: -100
-  })
-  .from('.main', {
-    duration: 0.5,
-    opacity: 0
-  })
+// const preload = gsap.timeline({
+//   paused: true
+// })
+// preload
+//   .to('.loader_bg', {
+//     delay: 0.5,
+//     duration: 0.5,
+//     opacity: 0,
+//     zIndex: -100
+//   })
+//   .from('.main', {
+//     duration: 0.5,
+//     opacity: 0
+//   })
 
-var id,
-  width = 0
+// var id,
+//   width = 0
 
-function loading() {
-  id = setInterval(frame, 50)
-}
+// function loading() {
+//   id = setInterval(frame, 50)
+// }
 
-function frame() {
-  if (width >= 100) {
-    clearInterval(id)
-    preload.play()
-    setTimeout(() => {
-      initialization()
-    }, 1000)
-  } else {
-    width++
-    const bar = document.getElementById('progress')
-    bar.style.width = width + '%'
-  }
-}
+// function frame() {
+//   if (width >= 100) {
+//     clearInterval(id)
+//     preload.play()
+//     setTimeout(() => {
+//       initialization()
+//     }, 1000)
+//   } else {
+//     width++
+//     const bar = document.getElementById('progress')
+//     bar.style.width = width + '%'
+//   }
+// }
 
 // TIMELINE GSAP QUE ANIMA OS CARDS NA ABERTURA DO SITE
-function initialization() {
-  gsap.from('.apple-grid', {
-    scale: 1.5,
-    opacity: 0,
-    duration: 1,
-    ease: 'expo.out'
-  })
+// function initialization() {
+//   gsap.from('.apple-grid', {
+//     scale: 1.5,
+//     opacity: 0,
+//     duration: 1,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.profile', {
-    x: -200,
-    opacity: 0,
-    duration: 3,
-    ease: 'expo.out'
-  })
+//   gsap.from('.profile', {
+//     x: -200,
+//     opacity: 0,
+//     duration: 3,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.stacks', {
-    x: 200,
-    opacity: 0,
-    delay: 0.2,
-    duration: 3,
-    ease: 'expo.out'
-  })
+//   gsap.from('.stacks', {
+//     x: 200,
+//     opacity: 0,
+//     delay: 0.2,
+//     duration: 3,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.resume', {
-    y: -200,
-    opacity: 0,
-    duration: 3,
-    delay: 0.5,
-    ease: 'expo.out'
-  })
+//   gsap.from('.resume', {
+//     y: -200,
+//     opacity: 0,
+//     duration: 3,
+//     delay: 0.5,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.contact', {
-    x: 200,
-    opacity: 0,
-    duration: 3.5,
-    ease: 'expo.out'
-  })
+//   gsap.from('.contact', {
+//     x: 200,
+//     opacity: 0,
+//     duration: 3.5,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.projects', {
-    scale: 1.2,
-    opacity: 0,
-    duration: 3,
-    ease: 'expo.out'
-  })
+//   gsap.from('.projects', {
+//     scale: 1.2,
+//     opacity: 0,
+//     duration: 3,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.select-mode', {
-    y: 200,
-    opacity: 0,
-    duration: 2.5,
-    ease: 'expo.out'
-  })
+//   gsap.from('.select-mode', {
+//     y: 200,
+//     opacity: 0,
+//     duration: 2.5,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.hello', {
-    y: -200,
-    opacity: 0,
-    delay: 0.2,
-    duration: 3,
-    ease: 'expo.out'
-  })
+//   gsap.from('.hello', {
+//     y: -200,
+//     opacity: 0,
+//     delay: 0.2,
+//     duration: 3,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.mail', {
-    x: 200,
-    opacity: 0,
-    delay: 0.3,
-    duration: 4,
-    ease: 'expo.out'
-  })
+//   gsap.from('.mail', {
+//     x: 200,
+//     opacity: 0,
+//     delay: 0.3,
+//     duration: 4,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.brand', {
-    y: -200,
-    opacity: 0,
-    delay: 0.7,
-    duration: 3,
-    ease: 'expo.out'
-  })
+//   gsap.from('.brand', {
+//     y: -200,
+//     opacity: 0,
+//     delay: 0.7,
+//     duration: 3,
+//     ease: 'expo.out'
+//   })
 
-  gsap.from('.welcome', {
-    y: -200,
-    opacity: 0,
-    duration: 3,
-    delay: 0.9,
-    ease: 'expo.out'
-  })
+//   gsap.from('.welcome', {
+//     y: -200,
+//     opacity: 0,
+//     duration: 3,
+//     delay: 0.9,
+//     ease: 'expo.out'
+//   })
 
-  // TIMELINE GSAP ANIMAÇÃO LISTA DE REDES SOCIAIS E CONTATOS
-  gsap.from('.clock', {
-    y: -50,
-    duration: 2,
-    opacity: 0,
-    ease: 'expo.out',
-    delay: 3
-  })
-  const linkList = document.querySelectorAll('.contact-list li')
-  linkList.forEach((list, i) => {
-    gsap.from(
-      list,
-      1,
-      {
-        y: 50,
-        opacity: 0,
-        ease: 'circ.out',
-        delay: i * 0.06
-      },
-      '-=.9'
-    )
-  })
+//   // TIMELINE GSAP ANIMAÇÃO LISTA DE REDES SOCIAIS E CONTATOS
+//   gsap.from('.clock', {
+//     y: -50,
+//     duration: 2,
+//     opacity: 0,
+//     ease: 'expo.out',
+//     delay: 3
+//   })
+//   const linkList = document.querySelectorAll('.contact-list li')
+//   linkList.forEach((list, i) => {
+//     gsap.from(
+//       list,
+//       1,
+//       {
+//         y: 50,
+//         opacity: 0,
+//         ease: 'circ.out',
+//         delay: i * 0.06
+//       },
+//       '-=.9'
+//     )
+//   })
 
-  // TIMELINE GSAP ANIMAÇÃO APRESENTAÇÃO DAS STACKS
-  const stackIcons = document.querySelectorAll('.stacks-icons .ico')
-  stackIcons.forEach((item, i) => {
-    gsap.from(
-      item,
-      1,
-      {
-        y: 50,
-        opacity: 0,
-        ease: 'circ.out'
-      },
-      '-=.8'
-    )
-  })
+//   // TIMELINE GSAP ANIMAÇÃO APRESENTAÇÃO DAS STACKS
+//   const stackIcons = document.querySelectorAll('.stacks-icons .ico')
+//   stackIcons.forEach((item, i) => {
+//     gsap.from(
+//       item,
+//       1,
+//       {
+//         y: 50,
+//         opacity: 0,
+//         ease: 'circ.out'
+//       },
+//       '-=.8'
+//     )
+//   })
 
-  // ANIMA O BOTÃO DE DESCRIÇÃO NA SESSÃO DE STACKS
-  gsap.from('.studing', {
-    y: 50,
-    opacity: 0,
-    duration: 1.5,
-    delay: 7.5,
-    ease: 'expo.out'
-  })
-  gsap.from('.interested', {
-    y: 50,
-    opacity: 0,
-    duration: 1.5,
-    delay: 8,
-    ease: 'expo.out'
-  })
-}
+//   // ANIMA O BOTÃO DE DESCRIÇÃO NA SESSÃO DE STACKS
+//   gsap.from('.studing', {
+//     y: 50,
+//     opacity: 0,
+//     duration: 1.5,
+//     delay: 7.5,
+//     ease: 'expo.out'
+//   })
+//   gsap.from('.interested', {
+//     y: 50,
+//     opacity: 0,
+//     duration: 1.5,
+//     delay: 8,
+//     ease: 'expo.out'
+//   })
+// }
 
-window.onload = function () {
-  loading()
-}
-// ANIMA SWIPPER JS
-gsap.from('.card-slide', {
-  scale: 0.5,
-  duration: 1.8,
-  ease: 'expo.out',
-  delay: 8,
-  opacity: 0
+// window.onload = function () {
+//   loading()
+// }
+// // ANIMA SWIPPER JS
+// gsap.from('.card-slide', {
+//   scale: 0.5,
+//   duration: 1.8,
+//   ease: 'expo.out',
+//   delay: 8,
+//   opacity: 0
+// })
+
+// gsap.from('.swiper-button-prev', {
+//   x: 50,
+//   duration: 2,
+//   ease: 'expo.out',
+//   delay: 10,
+//   opacity: 0,
+//   zIndex: -50
+// })
+
+// gsap.from('.swiper-button-next', {
+//   x: -50,
+//   duration: 2,
+//   ease: 'expo.out',
+//   delay: 10,
+//   opacity: 0,
+//   zIndex: -50
+// })
+// //  TIMELINE ANIMACAO APRESENTACAO CARD PROFILE
+// const tlprof = gsap.timeline()
+
+// tlprof
+//   .from('.profile-avatar', {
+//     y: 50,
+//     opacity: 0,
+//     duration: 1.5,
+//     delay: 7,
+//     ease: 'expo.inOut'
+//   })
+//   .from(
+//     '.me',
+//     {
+//       y: 30,
+//       duration: 1.5,
+//       ease: 'expo.inOut',
+//       opacity: 0
+//     },
+//     '-=1.2'
+//   )
+//   .from(
+//     '.profile-name h1',
+//     {
+//       y: 10,
+//       opacity: 0,
+//       ease: 'expo.inOut',
+//       duration: 1.5
+//     },
+//     '-=1.2'
+//   )
+//   .from(
+//     '.profile-name .role',
+//     {
+//       y: 15,
+//       opacity: 0,
+//       ease: 'expo.inOut',
+//       duration: 1.5
+//     },
+//     '-=1.1'
+//   )
+//   .from(
+//     '.profile-about p span',
+//     {
+//       y: 10,
+//       opacity: 0,
+//       ease: 'expo.inOut',
+//       duration: 1.3
+//     },
+//     '-=1'
+//   )
+
+const flipButton = document.querySelector('.plus-icon')
+const closeButton = document.querySelector('.close-icon')
+const flipCard = document.querySelector('.flip-card')
+
+flipButton.addEventListener('click', event => {
+  flipCard.classList.add('flip')
 })
 
-gsap.from('.swiper-button-prev', {
-  x: 50,
-  duration: 2,
-  ease: 'expo.out',
-  delay: 10,
-  opacity: 0,
-  zIndex: -50
+closeButton.addEventListener('click', () => {
+  flipCard.classList.remove('flip')
 })
 
-gsap.from('.swiper-button-next', {
-  x: -50,
-  duration: 2,
-  ease: 'expo.out',
-  delay: 10,
-  opacity: 0,
-  zIndex: -50
-})
-//  TIMELINE ANIMACAO APRESENTACAO CARD PROFILE
-const tlprof = gsap.timeline()
-
-tlprof
-  .from('.profile-avatar', {
-    y: 50,
-    opacity: 0,
-    duration: 1.5,
-    delay: 7,
-    ease: 'expo.inOut'
-  })
-  .from(
-    '.me',
-    {
-      y: 30,
-      duration: 1.5,
-      ease: 'expo.inOut',
-      opacity: 0
-    },
-    '-=1.2'
-  )
-  .from(
-    '.profile-name h1',
-    {
-      y: 10,
-      opacity: 0,
-      ease: 'expo.inOut',
-      duration: 1.5
-    },
-    '-=1.2'
-  )
-  .from(
-    '.profile-name .role',
-    {
-      y: 15,
-      opacity: 0,
-      ease: 'expo.inOut',
-      duration: 1.5
-    },
-    '-=1.1'
-  )
-  .from(
-    '.profile-about p span',
-    {
-      y: 10,
-      opacity: 0,
-      ease: 'expo.inOut',
-      duration: 1.3
-    },
-    '-=1'
-  )
-
-const buttonCopy = document.querySelector('.button-copy')
+const buttonCopyMail = document.querySelector('.button-copy')
 const checkmark = document.querySelector('.checkmark-svg')
 
 // FUNÇÃO QUE COPIA O EMAIL PARA A AREA DE TRANSFERENCIA DO USUARIO
 const copyEmailToClipboard = () => {
   const input = document.querySelector('.copy-mail')
-  const previousHTML = buttonCopy.innerHTML
+  const previousHTML = buttonCopyMail.innerHTML
 
   input.select()
   navigator.clipboard
     .writeText(input.value)
     .then(() => {
-      buttonCopy.classList.add('copied')
-      buttonCopy.textContent = `COPIADO!`
+      buttonCopyMail.classList.add('copied')
+      buttonCopyMail.textContent = `COPIADO!`
       setTimeout(() => {
-        buttonCopy.innerHTML = previousHTML
-        buttonCopy.classList.remove('copied')
+        buttonCopyMail.innerHTML = previousHTML
+        buttonCopyMail.classList.remove('copied')
       }, 2000)
     })
     .catch(err => {
@@ -317,7 +329,7 @@ const copyEmailToClipboard = () => {
 }
 
 // EVENTO DE CLIQUE NO BOTAO PARA COPIAR O EMAIL
-buttonCopy.addEventListener('click', copyEmailToClipboard)
+buttonCopyMail.addEventListener('click', copyEmailToClipboard)
 
 const toggleBtn = document.querySelector('.toggle-btn')
 const moonIcon = document.querySelector('.icon i')
