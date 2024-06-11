3f68131edf53281cc7ab23f6e898ad1cf0233f76
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2024-06-11 07:19:56 -0300

    chore: change style portfolio

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 const toggleButton = document.querySelector('.toggle-btn')
+const avatar = document.querySelector('.avatar')
+const mode = document.querySelector('.select-mode')
+
+const plusIcon = document.querySelector('.plus-icon')
+const closeIcon = document.querySelector('.close-icon')
+
+const flipContainer = document.querySelector('.profile')
 
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active')
+  avatar.classList.toggle('active')
+
+  const isActiveClass = toggleButton.classList.contains('active')
+
+  if (isActiveClass) {
+    setTimeout(() => {
+      mode.classList.toggle('night')
+    }, 800)
+
+    return
+  }
+
+  setTimeout(() => {
+    mode.classList.remove('night')
+  }, 800)
+})
+
+plusIcon.addEventListener('click', () => {
+  flipContainer.classList.add('active')
+})
+
+closeIcon.addEventListener('click', () => {
+  flipContainer.classList.remove('active')
 })
